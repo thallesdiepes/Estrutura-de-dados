8e754b73245825a176f7d79215deed61ac9d3d81
thallesdiepes <86641760+thallesdiepes@users.noreply.github.com>
2023-08-14 22:19:17 -0300

    Add files via upload

--- IBMEC/Estrutura_de_dados/ACs/AC-01.go
+++ IBMEC/Estrutura_de_dados/ACs/AC-01.go
@@ -2,48 +2,33 @@ package main
 
 import "fmt"
 
-//Exercício 01
-
 func ePrimo(n int) bool {
-	if n <= 1 {
-		fmt.Println("O número deve ser maior que 1")
-		return false
-	}
+    if n <= 1 {
+        fmt.Println("O número deve ser maior do que 1")
+        return false
+    }
 
-	divisores := []int{}
+    divisores := []int{}
 
-	for i := 2; i <= n/2; i++ {
-		if n%i == 0 {
-			divisores = append(divisores, i)
-		}
-	}
+    for i := 2; i <= n/2; i++ {
+        if n%i == 0 {
+            divisores = append(divisores, i)
+        }
+    }
 
-	if len(divisores) == 0 {
-		return true
-	}
+    if len(divisores) == 0 {
+        return true
+    }
 
-	fmt.Println("O número não é primo e é divisível por", divisores)
-	return false
+    fmt.Println("O número não é primo e é divisível por:", divisores)
+    return false
 }
-
-func main() {
-	num := 16
-	resultado := ePrimo(num)
-	if resultado {
-		fmt.Println(num, "é primo")
-	} else {
-		fmt.Sprintln(num, "não é primo")
-
-	}
-}
-
-// Exercício 2
 
 func Fibo(n int) int {
 	if n <= 0 {
 		fmt.Println("O número deve ser positivo")
+		return -1
 	}
-
 	if n == 1 || n == 2 {
 		return 1
 	}
@@ -52,23 +37,15 @@ func Fibo(n int) int {
 	termoAtual := 1
 
 	for i := 3; i <= n; i++ {
-		proximoTermo := termoAnterior + termoAtual
+		proxTermo := termoAnterior + termoAtual
 		termoAnterior = termoAtual
-		termoAtual = proximoTermo
+		termoAtual = proxTermo
 	}
 
 	return termoAtual
 }
 
-func main() {
-	n := 6
-	resultado := Fibo(n)
-	fmt.Printf("O %dº número na série de Fibonacci é %d\n", n, resultado)
-}
-
-//Exercício 3
-
-func diaSemana(n int) {
+func diaDaSemana(n int) {
 	switch n {
 	case 1:
 		fmt.Println("Domingo")
@@ -89,14 +66,6 @@ func diaSemana(n int) {
 	}
 }
 
-func main() {
-	n := 4
-	fmt.Printf("O número %d corresponde ao dia: ", n)
-	diaDaSemana(n)
-}
-
-//Exercício 4
-
 func eBissexto(ano int) bool {
 	if ano%4 != 0 {
 		return false
@@ -109,10 +78,25 @@ func eBissexto(ano int) bool {
 }
 
 func main() {
-	ano := 2000
-	if eBissexto(ano) {
-		fmt.Println(ano, "é bissexto")
-	} else {
-		fmt.Println(ano, "não é bissexto")
-	}
+    fmt.Println("Verificando se o número é primo:")
+    num := 12
+    ePrimo(num)
+
+    fmt.Println("\nCalculando o n-ésimo número na série de Fibonacci:")
+    n := 6
+    resultado := Fibo(n)
+    fmt.Printf("O %dº número na série de Fibonacci é %d\n", n, resultado)
+
+    fmt.Println("\nExibindo o dia correspondente da semana:")
+    dia := 4
+    diaDaSemana(dia)
+
+    fmt.Println("\nVerificando se o ano é bissexto:")
+    ano := 2000
+    if eBissexto(ano) {
+        fmt.Println(ano, "é bissexto")
+    } else {
+        fmt.Println(ano, "não é bissexto")
+    }
 }
+
